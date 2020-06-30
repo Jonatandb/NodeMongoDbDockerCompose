d91827ced1e90b2a1c2c6b824703f2d80fca0f19
Jonatandb <jonatandb@gmail.com>
2020-06-29 21:08:16 -0300

    Configuro opcinoes de conexión de Mongoose para que no tire warnings en la consola

--- src/database.js
+++ src/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://mongo/mydatabase")
+  .connect("mongodb://mongo/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((db) => console.log("Db connected to:", db.connection.host))
   .catch((err) => console.error(err));
