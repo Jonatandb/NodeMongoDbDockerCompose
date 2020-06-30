b578d5565bfbcd34c6578814002c3eb8ddf244d9
Jonatandb <jonatandb@gmail.com>
2020-06-29 21:17:58 -0300

    Actualizo cadena de conexión a servidor MongoDb ya que lleva el mismo nombre que el servicio en el archivo docker-compose.yml

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://mongo/mydatabase", {
+  .connect("mongodb://mongoserver/mydatabase", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
